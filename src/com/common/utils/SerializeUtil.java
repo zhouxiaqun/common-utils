35a47009d39f8914dfe32e24e0dac8b9f20b8454
zhouxiaqun <zxq3203@163.com>
2016-08-10 09:53:39 +0800

    序列化和反序列化

---
+++ src/com/common/utils/SerializeUtil.java
@@ -0,0 +1,40 @@
+package com.common.utils;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public class SerializeUtil {
+
+	    public static byte[] serialize(Object object) {
+	        ObjectOutputStream oos = null;
+	        ByteArrayOutputStream baos = null;
+	        try {
+	            // 序列化
+	            baos = new ByteArrayOutputStream();
+	            oos = new ObjectOutputStream(baos);
+	            oos.writeObject(object);
+	            byte[] bytes = baos.toByteArray();
+	            return bytes;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return null;
+	    }
+
+	    public static Object unserialize(byte[] bytes) {
+	        try {
+	            // 反序列化
+	            ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new ByteArrayInputStream(bytes)));
+	            return ois.readObject();
+	        } catch (Exception e) {
+	        	e.printStackTrace();
+	        }
+	        return null;
+	    }
+
+
+}
